b2c7aa292079c96aa3a77c87a5bd5529fe939fdf
Picker Lee <450994392@qq.com>
2016-10-19 23:24:22 +0800

    检查当前会话

--- src/routers/account/check.js
+++ src/routers/account/check.js
@@ -7,10 +7,7 @@ router.get('account/check', async function(ctx, next) {
 	// 登录设置用户会话
 	const user = ctx.session.user;
 	if (!user) {
-		return ctx.customResponse.success({
-			code: 401,
-			message: '未登录！'
-		});
+		return ctx.customResponse.error('未登录！', 200);
 	}
 
 	ctx.customResponse.success({
