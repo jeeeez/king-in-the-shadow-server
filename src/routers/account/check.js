f0647688a100f5e5a32461751b6b36e63da977a3
Picker Lee <450994392@qq.com>
2016-10-27 23:35:04 +0800

    account check

--- src/routers/account/check.js
+++ src/routers/account/check.js
@@ -1,12 +1,13 @@
 import router from '../router';
 import User from '../../models/user';
-import accountAuth from '../../middlewares/auth';
+// import accountAuth from '../../middlewares/auth';
 
 // 查看用户登录状态
-router.get('account/check', accountAuth.user, async function(ctx, next) {
+router.get('account/check', async function(ctx, next) {
 
 	// 登录设置用户会话
 	const user = ctx.session.user;
+	if (!user) return ctx.customResponse.success('未登录');
 
 	ctx.customResponse.success({
 		id: user._id,
