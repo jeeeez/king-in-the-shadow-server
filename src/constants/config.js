0466fc2082c9e37de19237372c69b8db6712fedf
Picker Lee <450994392@qq.com>
2016-11-23 20:36:46 +0800

    区分开发/生产环境的mongodb路径

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,9 @@
+// import argv from 'yargs';
+
+const NODE_ENV = process.env.NODE_ENV;
+
 export default {
-	mongodb: 'mongodb://127.0.0.1:27017/test',
-	// mongodb: 'mongodb://23.105.209.85:27017/test',
+	mongodb: NODE_ENV === 'development' ? 'mongodb://127.0.0.1:27017/test' : 'mongodb://23.105.209.85:27017/test',
 
 	// 每日最多能创建的邀请码
 	maxInvitationAmount: 10
