89a4ab83d814ef7a02489f64f502027c179173bb
Picker Lee <450994392@qq.com>
2016-12-14 10:27:29 +0800

    为session设置最后使用时间，当系统中的session数量超过设置的阈值时删除最久未使用的session

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -15,28 +15,40 @@ export default (ctx, next) => {
 	ctx.session = {
 		get user() {
 			const token = ctx.headers['king-token'];
-			return SESSIONS[token];
+			const session = SESSIONS[token];
+			if (session) {
+				// 每次使用session都会重新激活
+				session.date = +new Date();
+				return session.user;
+			}
+
+			// return undefined;
 		},
 
-		set user(session) {
-			// 如果session为空则表示删除当前token对应的session
-			if (!session) {
+		set user(user) {
+			// 如果user为空则表示删除当前token对应的session
+			if (!user) {
 				const token = ctx.headers['king-token'];
 				if (!token) return;
 
 				delete SESSIONS[token];
 				return;
 			}
-			// 当系统的session个数超出最大阈值，则自动删除最前面的session
-			// 虽然Object中的属性是无序（不可靠）的，但是如果key值为String类型，一般还是会以添加顺序为序
-			// --TODO:为session设置最后使用时间，按该时间的倒序排序，优先删除最久没使用的session
+
+			// 当系统的session个数超出最大阈值，则自动最久未使用的session
 			const tokens = Object.keys(SESSIONS);
 			if (tokens.length >= MAX_SESSION_AMOUNT) {
-				delete SESSIONS[tokens[0]];
+				const oldestSessionToken = tokens.sort((token1, token2) => {
+					return SESSIONS[token1].date - SESSIONS[token2].date;
+				})[0];
+				delete SESSIONS[oldestSessionToken];
 			}
 
 			ctx.session.token = generateToken();
-			SESSIONS[ctx.session.token] = session;
+			SESSIONS[ctx.session.token] = {
+				user,
+				date: +new Date()
+			};
 		}
 	};
 	return next();
