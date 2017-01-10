cce309f4be354302422b5e2daf5fe89dc05f2bc9
Picker Lee <450994392@qq.com>
2017-01-10 23:41:54 +0800

    数据库连接

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,12 +2,9 @@
 
 const NODE_ENV = process.env.NODE_ENV;
 
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PSW;
 
 export default {
-	mongodb: NODE_ENV === `mongodb://${DB_USERNAME}:${DB_PASSWORD}@` +
-		'development' ? '127.0.0.1:27017/test' : 'mongodb://23.105.209.85:27017/test',
+	mongodb: NODE_ENV === 'development' ? 'mongodb://127.0.0.1:27017/test' : 'mongodb://23.105.209.85:27017/test',
 
 	// 每日最多能创建的邀请码
 	maxInvitationAmount: 100
