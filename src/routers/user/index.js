217dec070dbd03bfb2a8acf254de6cdbdbd53c53
JFBOY <450994392@qq.com>
2017-09-28 20:42:45 +0800

    feat:添加管理员为用户充值时间的功能

--- src/routers/user/index.js
+++ src/routers/user/index.js
@@ -28,3 +28,43 @@ router.get('users', accountAuth.admin, async function(ctx, next) {
 		ctx.customResponse.error(error.message);
 	}
 });
+
+
+/**
+ * 管理员为用户充值天数
+ */
+router.post('users/:userID/activate',
+	accountAuth.superAdmin,
+	ParameterValidator.body({
+		days: {
+			required: '充值天数不能为空',
+			pattern: /[1-9][0-9]*/,
+		}
+	}),
+	async function(ctx, next) {
+		try {
+			const userID = ctx.params.userID;
+			const { days } = ctx.request.body;
+			const user = await User.get({ _id: userID });
+			if (!user) {
+				return ctx.customResponse.error('用户不存在');
+			}
+
+			const prexExprieDate = Math.max((user.expireDate || 0), Date.now());
+			const daysMills = 24 * 60 * 60 * 1000 * days;
+
+			const expireDate = prexExprieDate + daysMills;
+
+			await User.update({ _id: userID }, { expireDate });
+
+			if ((user.expireDate || 0) < Date.now()) {
+				ShadowrocksService.updateOnePort(user.port, user.auth);
+			}
+
+			// 返回用户当前的有效时间
+			ctx.customResponse.success({ expireDate });
+
+		} catch (error) {
+			ctx.customResponse.error(error.message);
+		}
+	});
